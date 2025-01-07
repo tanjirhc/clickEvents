8b43ba69378e727232158f5f46a5c2e9121a2bca
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-07 15:21:19 +0600

    onClick event handler

--- src/ProfilePicture.jsx
+++ src/ProfilePicture.jsx
@@ -2,7 +2,10 @@
 function ProfilePicture() {
   
     const imageUrl = './src/assets/tanjir.jpg';
+    const handleClick = () => {
+      console.log('Image clicked');
+    }
 
-    return(<img src={imageUrl}></img>);
+    return(<img onClick={handleClick} src={imageUrl}></img>);
 }   
 export default ProfilePicture;
