61bd97e089a702ea736d54e4c07685bd8283cf25
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-07 13:27:21 +0600

    React clickEvents

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Button from './Button'
+
+function App() {
+  
+  return (
+    <>
+    <Button />
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Button.jsx
@@ -0,0 +1,5 @@
+function Button(){
+    const handleClick = () => console.log('Button clicked');    
+    return <button onClick={handleClick}>Click me</button>;
+}
+export default Button;
